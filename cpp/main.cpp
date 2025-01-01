a424e14f289e54a01c5a2a55204ba0962e1b05b1
Petar Koretic <petar.koretic@gmail.com>
2025-01-01 20:42:49 +0100

    migrate to cmake
    not a fan of this but it's what Qt has moved away to
    
    Signed-off-by: Petar Koretic <petar.koretic@gmail.com>

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -11,12 +11,16 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
 
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
+                     &app, []() { QCoreApplication::exit(-1); },
+                     Qt::QueuedConnection);
+
     // expose C++ classes to QML
     engine.addImageProvider(QLatin1String("icon"), new ImageProvider());
     engine.rootContext()->setContextProperty("__platform", &Platform::instance());
 
     // load main file
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.loadFromModule("LetiHomeModule", "Main");
 
     return app.exec();
 }
